cedfad8d011411763287251b70919fabe2c8c6c0
Hersey <helane1996@gmail.com>
2024-04-09 22:22:12 -0400

    added fetch request to get coordinates of user city and stored the city object to an Array

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 const searchBarEl = document.getElementById(`search-bar`);
 const submitBtnEl = document.getElementById(`submit-button`);
 let searchHistory = JSON.parse(localStorage.getItem(`searches`)) || [];
-console.log(searchBarEl);
-console.log(submitBtnEl);
+let cityArray = [];
+
+// My Weather API key --- 26fdb0f1c088de04bafa78b85c21be83
 
 function handleSearchSubmit(event) {
   //make an if statement for if the user doesn't input a valid city name
@@ -16,7 +17,33 @@ function handleSearchSubmit(event) {
 
   localStorage.setItem(`searches`, JSON.stringify(searchHistory));
 
-  console.log(searchHistory);
+  //   getWeatherDataApi()
+  submitCityNameApiRequest();
 }
+
+function submitCityNameApiRequest() {
+  const cityNameUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchBarEl.value}&limit=1&appid=26fdb0f1c088de04bafa78b85c21be83`;
+
+  return fetch(cityNameUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (let i = 0; i < data.length; i++) {
+        // const lat = data[i].lat;
+        // const lon = data[i].lon;
+        cityArray.push(data[i]);
+      }
+    });
+}
+
+// async function getWeatherDataApi() {
+//     let cityCoords = await submitCityNameApiRequest();
+//     console.log(cityCoords);
+// }
+
+function handleForecastPopulation(data) {}
+
+function handleCurrentWeather(data) {}
 
 submitBtnEl.addEventListener(`click`, handleSearchSubmit);
