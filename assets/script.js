426a3212acf59a6690777d54d0600cf16453f284
Hersey <helane1996@gmail.com>
2024-04-10 17:50:50 -0400

    begun adding dynamic elements for 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -22,7 +22,7 @@ function handleSearchSubmit(event) {
 }
 
 function submitCityNameApiRequest() {
-  const cityNameUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchBarEl.value}&limit=1&appid=26fdb0f1c088de04bafa78b85c21be83`;
+  const cityNameUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchBarEl.value}&limit=1&appid=3425d908dbea20e1649805cca8ffecfb`;
 
   return fetch(cityNameUrl)
     .then(function (response) {
@@ -31,7 +31,7 @@ function submitCityNameApiRequest() {
     .then(function (data) {
       for (let i = 0; i < data.length; i++) {
         cityArray.push(data[i]);
-        // console.log(cityArray)
+        console.log(cityArray);
       }
     });
 }
@@ -40,23 +40,37 @@ async function fetchWeatherDataApi() {
   await submitCityNameApiRequest();
   let cityLat = cityArray[0].lat;
   let cityLon = cityArray[0].lon;
-  const cityWeatherUrl = `api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=26fdb0f1c088de04bafa78b85c21be83` 
+  const cityWeatherUrl = `api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=3425d908dbea20e1649805cca8ffecfb`;
   //to call the CURRENT WEATHER just run this same url replacing forecast with weather
   console.log(cityWeatherUrl);
 
-//   fetch(cityWeatherUrl)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data)
-//   })
-
-//   cityArray.length = 0;
+  return fetch(cityWeatherUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      cityArray.length = 0;
+    });
 }
 
-function handleForecastPopulation(data) {}
-
 function handleCurrentWeather(data) {}
+
+function handleForecastCardPopulation(data) {
+  const forecastContainerEl = document.getElementById(`forecast-container`);
+  const forecastCardEl = document.createElement(`div`);
+  const forecastCardBodyEl = document.createElement(`div`);
+  const forecastCardTitleEl = document.createElement(`h5`);
+  const forecastCardSubtitleEl = document.createElement(`h6`);
+  const forecastCardTempEl = document.createElement(`p`);
+  const forecastCardWindEl = document.createElement(`p`);
+  const forecastCardHumidEl = document.createElement(`p`);
+
+  forecastCardEl.classList.add("card", "five-day-box", "m-auto")
+  forecastCardEl.style.add("width: 15rem;", "height: 20rem;")
+  forecastCardBodyEl.classList.add(`card-body`)
+  forecastCardTitleEl.classList.add("card-title", "pt-2", "pb-3")
+
+}
 
 submitBtnEl.addEventListener(`click`, handleSearchSubmit);
